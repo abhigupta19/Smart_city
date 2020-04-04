b28d0f183dc838a08a72e823297d3a40bb9f7af6
Abhinav  Gupta <gabhi8740@gmail.com>
2020-04-04 23:48:55 +0530

    Initial commit

---
+++ app/src/main/java/com/sar/user/smart_city/MainScreen.java
@@ -0,0 +1,19 @@
+package com.sar.user.smart_city;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainScreen extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener authStateListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+}
